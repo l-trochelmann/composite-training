ac0b6471341deb9c943de216c6c6286e0e10b826
bay0567 <bay0567@studium.uni-hamburg.de>
2023-04-26 23:21:14 +0200

    delete irrelevant sigmas

--- configs/_base_/datasets/composite.py
+++ configs/_base_/datasets/composite.py
@@ -10,11 +10,6 @@ dataset_info = dict(
         homepage='',
     ),
 
-    # legacy keypoints order:   
-    # coco: [nose, left_eye, right_eye, left_ear, right_ear, left_shoulder, right_shoulder, left_elbow, right_elbow, left_wrist, right_wrist, left_hip, right_hip, left_knee, right_knee, left_ankle, right_ankle]  
-    # aic:  [right_shoulder, right_elbow, right_wrist, left_shoulder, left_elbow, left_wrist, right_hip, right_knee, right_ankle, left_hip, left_knee, left_ankle, head_top, neck]
-    # composite keypoint order:
-    # [head_top, left_eye, right_eye, left_ear, right_ear, nose, neck, left_shoulder, right_shoulder, left_elbow, right_elbow, left_wrist, right_wrist, left_hip, right_hip, left_knee, right_knee, left_ankle, right_ankle]
     keypoint_info = {
         0: dict(
             name='head_top',
@@ -134,54 +129,32 @@ dataset_info = dict(
 
     # common links in purple, aic-only links in blue, coco-only links in red
     skeleton_info={
-        0: dict(link=('right_wrist', 'right_elbow'), id=0, color=[128, 0, 128]),
-        1: dict(link=('right_elbow', 'right_shoulder'), id=1, color=[128, 0, 128]),
-        2: dict(link=('left_shoulder', 'left_elbow'), id=2, color=[255, 128, 255]),
-        3: dict(link=('left_elbow', 'left_wrist'), id=3, color=[255, 128, 255]),
-        4: dict(link=('right_ankle', 'right_knee'), id=4, color=[128, 0, 128]),
-        5: dict(link=('right_knee', 'right_hip'), id=5, color=[128, 0, 128]),
+        0: dict(link=('right_wrist', 'right_elbow'), id=0, color=[255, 0, 255]),
+        1: dict(link=('right_elbow', 'right_shoulder'), id=1, color=[255, 0, 255]),
+        2: dict(link=('left_shoulder', 'left_elbow'), id=2, color=[255, 0, 255]),
+        3: dict(link=('left_elbow', 'left_wrist'), id=3, color=[255, 0, 255]),
+        4: dict(link=('right_ankle', 'right_knee'), id=4, color=[255, 0, 255]),
+        5: dict(link=('right_knee', 'right_hip'), id=5, color=[255, 0, 255]),
         6: dict(link=('right_hip', 'left_hip'), id=6, color=[255, 0, 255]),
-        7: dict(link=('left_hip', 'left_knee'), id=7, color=[255, 128, 255]),
-        8: dict(link=('left_knee', 'left_ankle'), id=8, color=[255, 128, 255]),
-        9: dict(link=('right_shoulder', 'right_hip'), id=9, color=[128, 0, 128]),
-        10: dict(link=('left_shoulder', 'left_hip'), id=10, color=[255, 128, 255]),
+        7: dict(link=('left_hip', 'left_knee'), id=7, color=[255, 0, 255]),
+        8: dict(link=('left_knee', 'left_ankle'), id=8, color=[255, 0, 255]),
+        9: dict(link=('right_shoulder', 'right_hip'), id=9, color=[255, 0, 255]),
+        10: dict(link=('left_shoulder', 'left_hip'), id=10, color=[255, 0, 255]),
+        11: dict(link=('left_shoulder', 'right_shoulder'), id=11, color=[255, 0, 255]),
 
-        11: dict(link=('right_shoulder', 'neck'), id=11, color=[0, 0, 128]),
-        12: dict(link=('neck', 'left_shoulder'), id=12, color=[128, 128, 255]),
-        13: dict(link=('head_top', 'neck'), id=13, color=[0, 0, 255]),
+        12: dict(link=('right_shoulder', 'neck'), id=12, color=[0, 0, 255]),
+        13: dict(link=('neck', 'left_shoulder'), id=13, color=[0, 0, 255]),
+        14: dict(link=('head_top', 'neck'), id=14, color=[0, 0, 255]),
 
-        14: dict(link=('left_eye', 'right_eye'), id=14, color=[255, 0, 0]),
-        15: dict(link=('nose', 'left_eye'), id=15, color=[255, 128, 128]),
-        16: dict(link=('nose', 'right_eye'), id=16, color=[128, 0, 0]),
-        17: dict(link=('left_eye', 'left_ear'), id=17, color=[255, 128, 128]),
-        18: dict(link=('right_eye', 'right_ear'), id=18, color=[128, 0, 0]),
-        19: dict(link=('left_ear', 'left_shoulder'), id=19, color=[255, 128, 128]),
-        20: dict(link=('right_ear', 'right_shoulder'), id=20, color=[128, 0, 0]),
-        21: dict(link=('left_shoulder', 'right_shoulder'), id=21, color=[255, 0, 0])
+        15: dict(link=('left_eye', 'right_eye'), id=15, color=[255, 0, 0]),
+        16: dict(link=('nose', 'left_eye'), id=16, color=[255, 0, 0]),
+        17: dict(link=('nose', 'right_eye'), id=17, color=[255, 0, 0]),
+        18: dict(link=('left_eye', 'left_ear'), id=18, color=[255, 0, 0]),
+        19: dict(link=('right_eye', 'right_ear'), id=19, color=[255, 0, 0]),
+        20: dict(link=('left_ear', 'left_shoulder'), id=20, color=[255, 0, 0]),
+        21: dict(link=('right_ear', 'right_shoulder'), id=21, color=[255, 0, 0])
     },
 
-    # for the composite joint weights, coco and aic weights did already match for the common weights, 
-    #   and for non-common weights the respective original weight was used 
     joint_weights=[1., 1., 1., 1., 1., 1., 1., 1., 1., 1.2, 1.2, 1.5, 1.5, 1., 1., 1.2, 1.2, 1.5, 1.5],
 
-
-    # todo: update sigmas
-
-    # 'https://github.com/AIChallenger/AI_Challenger_2017/blob/master/'
-    # 'Evaluation/keypoint_eval/keypoint_eval.py#L50'
-    # delta = 2 x sigma
-    sigmas=[0.01291456, 0.025, 0.025, 0.035, 0.035, 0.026, 0.01236173, 0.0421436598093409, 0.04210130459424179, 
-    0.036182338706306494, 0.036422573620422026, 0.030655928110977716, 0.02977796138264048, 0.06210660926448471, 
-    0.06244901144807664, 0.05015083471299594, 0.05167741980833102, 0.045610413654848594, 0.043919830609138295])
-
-"""
-Composite sigmas if visibility 1 AND 2 are being used and 0 is disregarded:
-[0.01291456, 0.025, 0.025, 0.035, 0.035, 0.026, 0.01236173, 0.0421436598093409, 0.04210130459424179, 
-0.036182338706306494, 0.036422573620422026, 0.030655928110977716, 0.02977796138264048, 0.06210660926448471, 
-0.06244901144807664, 0.05015083471299594, 0.05167741980833102, 0.045610413654848594, 0.043919830609138295]
-
-Composite sigmas if visibility of ONLY 2 is being used and 0 and 1 disregarded:
-[0.01291456, 0.025, 0.025, 0.035, 0.035, 0.026, 0.01236173, 0.04341598377117178, 0.043217163135518086, 
-0.037765465831840456, 0.037679646038876156, 0.03254687730621457, 0.03160757075215834, 0.06414867045130725, 
-0.06425738531941605, 0.05124276488481217, 0.052694678251659736, 0.04677465004102636, 0.045186607388038535]
-"""
+    sigmas=[])
